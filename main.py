447e70efddadc4cf0556d47a420c2e674fd798c4
Bshrek <84413865+Bshrek@users.noreply.github.com>
2023-01-05 20:49:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 #main commands
 
-import os
-
-def list_files(startpath):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('{}{}'.format(subindent, f))
-           
-list_files('/opt')
-
 from cgitb import text
 from datetime import date, datetime
 from email import message

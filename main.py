48e6e6eec35934a55561f83c953979d123e12a36
Bshrek <84413865+Bshrek@users.noreply.github.com>
2023-01-05 20:21:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #main commands
 
-import os
-print('Get current working directory : ', os.getcwd())
+import inspect, os
+print (inspect.getfile(inspect.currentframe())) # script filename (usually with path)
+print (os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))) # script directory
 
 
 from cgitb import text
